1a09e6a2356dfcf9cc818aacda2fb96613cb3fc4
Eelko van Breda <eelko.vanbreda@evondos.com>
2025-01-27 15:36:53 +0100

    Initial fish settings

---
+++ config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias vi=nvim
+alias tm='tmux new-session -s "$(git rev-parse --abbrev-ref HEAD)"'
+alias bfm=build_for_medido
+alias gco='git checkout'
+alias lg=lazygit
+
+set PATH $PATH $HOME/.local/bin
+
+# pyenv init
+if command -v pyenv 1>/dev/null 2>&1
+  pyenv init - | source
+end
